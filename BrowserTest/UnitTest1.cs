b70a9cfccdf7a66f051393ee692ff11a86f672cf
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-10-22 00:16:40 +0200

    Opgaven lavet færdigt med kommentare til koden.

--- BrowserTest/UnitTest1.cs
+++ BrowserTest/UnitTest1.cs
@@ -10,46 +10,59 @@ namespace collectWordsTest
     [TestClass]
     public class UnitTest1
     {
-        private static readonly string DriverDirectory = "C:\\Webdrivers";
+        // Sti til ChromeDriver eksekverbar fil
+        private static readonly string DriverDirectory = "C:/seleniumDrivers/chromedriver-win64/chromedriver.exe";
+        // WebDriver instans
         private static IWebDriver _driver;
 
+        // Initialiserer WebDriver før nogen tests køres
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
             _driver = new ChromeDriver(DriverDirectory);
         }
 
+        // Rydder op efter alle tests er kørt
         [ClassCleanup]
         public static void TearDown()
         {
-            //_driver.Dispose();
+            _driver.Dispose(); // skal bruges til at frigive ressourcer
         }
 
+        // Testmetode der tester forskellige funktionaliteter på en lokal HTML-side
         [TestMethod]
         public void TestMethod1()
         {
-            //_driver.Navigate().GoToUrl("http://localhost:5503/index.htm");
-            _driver.Navigate().GoToUrl("file:///C:/users/gympc/desktop/3sem/CollectWords/index.html");
+            // Navigerer til den lokale HTML-side
+            _driver?.Navigate().GoToUrl("file:///C:/javatest/CollectWords/index.html");
+            // Tjekker at sidens titel er "CollectWords"
             Assert.AreEqual("CollectWords", _driver.Title);
 
+            // Finder input elementet og indtaster "Test1"
             IWebElement inputElement1 = _driver.FindElement(By.Id("wordInput"));
-            inputElement1.SendKeys("abc");
+            inputElement1.SendKeys("Test1");
 
+            // Finder og klikker på gem-knappen
             IWebElement saveButton = _driver.FindElement(By.Id("saveButton"));
             saveButton.Click();
 
+            // Finder og klikker på vis-knappen
             IWebElement showButton = _driver.FindElement(By.Id("showButton"));
             showButton.Click();
 
+            // Finder resultat elementet og tjekker at teksten er "Test1"
             IWebElement resultElement = _driver.FindElement(By.Id("output"));
-            Assert.AreEqual("abc", resultElement.Text);
+            Assert.AreEqual("Test1", resultElement.Text);
 
+            // Rydder input, indtaster "Test2", og gentager gem og vis handlingerne
             inputElement1.Clear();
-            inputElement1.SendKeys("def");
+            inputElement1.SendKeys("Test2");
             saveButton.Click();
             showButton.Click();
-            Assert.AreEqual("abc,def", resultElement.Text);
+            // Tjekker at resultatet nu indeholder "Test1, Test2"
+            Assert.AreEqual("Test1, Test2", resultElement.Text);
 
+            // Finder og klikker på ryd-knappen, og tjekker at resultatet er "empty"
             IWebElement clearButton = _driver.FindElement(By.Id("clearButton"));
             clearButton.Click();
             showButton.Click();
